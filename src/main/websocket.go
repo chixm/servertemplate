badc8a8b7b93c184571efa0b9d478ab33f043a6a
chixm <makoto0072104@yahoo.co.jp>
2019-03-20 17:00:13 +0900

    websocket部分基礎

---
+++ src/main/websocket.go
@@ -0,0 +1,108 @@
+package main
+
+// websocket connection handling of server
+
+import (
+	"net/http"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader websocket.Upgrader
+
+func InitializeWebSocket() {
+	upgrader = websocket.Upgrader{CheckOrigin: checkOriginHost}
+}
+
+// Entry Point of WebSocket Request
+func ws(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil) //upgrade from http to ws
+	if err != nil {
+		logger.Errorln(err)
+		return
+	}
+	defer conn.Close()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	// check websocket connection is kept connected.
+	observeChan := connectionObserver(conn)
+	// wait for client message to arrive.
+	msgChan := messageListener(conn)
+
+	// if observer or messageLister Ends, Finish WebSocket Connection.
+	for {
+		select {
+		case <-observeChan:
+			wg.Done()
+		case <-msgChan:
+			wg.Done()
+		default:
+			time.Sleep(100 * time.Millisecond)
+		}
+	}
+	wg.Wait()
+}
+
+// check websocket is alive in interval
+func connectionObserver(conn *websocket.Conn) <-chan int {
+	obs := make(chan int)
+
+	go func() {
+		defer close(obs)
+	observeLoop:
+		for {
+			var pingMessage = `server ping`
+			// Do ping pong each 5 seconds to check socket is connected.
+			err := conn.WriteControl(websocket.PingMessage, []byte(pingMessage), time.Now().Add(5*time.Second))
+			if err != nil {
+				break observeLoop
+			}
+		}
+		obs <- 0
+	}()
+
+	return obs
+}
+
+func messageListener(conn *websocket.Conn) <-chan int {
+	msgChan := make(chan int)
+	//handle every client message here.
+	go func() {
+	messageLoop:
+		for {
+			mt, msg, err := conn.ReadMessage()
+			if err != nil {
+				logger.Errorln(err)
+				break messageLoop
+			}
+			switch mt {
+			case websocket.BinaryMessage:
+				binaryMessageReceiver(msg)
+			case websocket.TextMessage:
+				textMessageReceiver(msg)
+			case websocket.PingMessage: //Do Ping Pong.
+				pingMessageReceiver(msg)
+			case websocket.PongMessage: //Nothing to Do.
+				pongMessageReceiver(msg)
+			default:
+				logger.Errorln(`Unknown Message Type detected.`)
+				break messageLoop
+			}
+		}
+		msgChan <- 0
+	}()
+	return msgChan
+}
+
+// check whether websocket is connected from right host
+func checkOriginHost(r *http.Request) bool {
+	// for example, you can't connect this websocket server from google.
+	if strings.Contains(r.Host, `google.com`) {
+		return false
+	}
+	return true
+}
---
+++ src/main/websocketMessageReceiver.go
@@ -0,0 +1,19 @@
+package main
+
+// handles messages from websocket client
+
+func textMessageReceiver(msg []byte) {
+
+}
+
+func binaryMessageReceiver(msg []byte) {
+
+}
+
+func pingMessageReceiver(msg []byte) {
+
+}
+
+func pongMessageReceiver(msg []byte) {
+
+}
